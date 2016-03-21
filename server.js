872b55e1dd2d5f86e397fd01c3f6c6883d38ccb5
emwykyu <emwykyu@live.com>
2016-03-21 05:58:28 +0000

    finished baseGame prof, more work on auth

--- server.js
+++ server.js
@@ -2,37 +2,38 @@
  * Created by Emwykyu on 13/03/2016.
  */
 
-//server.js
+// server.js
 
-//set-up ================================================
-
-var express = require('express');
-var app = express;
-var port = _process.env.PORT || 8080;
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
-var flash = require('connect-flash');
+var flash    = require('connect-flash');
 
-var morgan = require('morgan');
-var cookieParser = require('cookie-parse');
-var bodyParser = require('body-parse');
-var session = require('express-session');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
-var configDB = require('./config/database.js;'); //CHANGE PATH IF NOT WORKING!
+var configDB = require('./config/database.js');
 
-//config ==================================================
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
 
-//require('./config/passport')(passport); //pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
-//set up express application
-app.use(morgan('dev')); //log every request to console
-app.use(cookieParser()); //read cookies (for auth)
-app.use(bodyParser()); //get information from html forms
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
 
-app.set('view engine', 'ejs'); //set ejs for templating
+app.set('view engine', 'ejs'); // set up ejs for templating
 
-//required for passport
-app.use(session({ secret: 'gw2fyp' })); // session secret
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
@@ -42,4 +43,4 @@ require('./app/routes.js')(app, passport); // load our routes and pass in our ap
 
 // launch ======================================================================
 app.listen(port);
-console.log('auth listening on port:  ' + port);
+console.log('The magic happens on port ' + port);
