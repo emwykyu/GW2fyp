db77a7eda9f9e5746fe8dd3b3fd58a31898e8f28
emwykyu <emwykyu@live.com>
2016-04-10 01:50:18 +0100

    aesthetic changes and bug fixes
    updated thumbnails, fixes to bugs and visuals

--- routes/myGW2.js
+++ routes/myGW2.js
@@ -5,8 +5,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET myGW2 page. */
-router.get('/', function(req, res, next) {
+router.get('/', ensureAuthenticated, function(req, res, next) {
     res.render('myGW2', { title: 'GW2 LeyLine:: MY GW2' });
 });
+
+function ensureAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    } else {
+        //req.flash('error_msg','You are not logged in');
+        res.redirect('/users/login');
+    }
+}
 
 module.exports = router;
